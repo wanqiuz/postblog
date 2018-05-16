5e3e41b303acb6ee9036cf5c7d008c073bfc294e
wanqiuz <zhaotree@126.com>
2018-05-17 02:16:35 +0800

    change the configuration

--- src/main/java/com/ioutstanding/postblog/ArticleController.java
+++ src/main/java/com/ioutstanding/postblog/ArticleController.java
@@ -1,10 +1,7 @@
 package com.ioutstanding.postblog;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
